92016ba349292773219f4ee9071f3e9ca64ca693
Guilherme Gurgel <gguigurgel@gmail.com>
2019-04-21 11:43:58 -0300

    Primeiro Commit

---
+++ GDA/src/main/java/com/example/GDA/GdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.GDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdaApplication.class, args);
+	}
+
+}
